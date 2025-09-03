b50e8e7a9bd8eea388a7b0f411692efad1f46760
Zdravko Bonev <zdravko.bonev@unrealsoft.bg>
2025-09-03 14:41:20 +0300

    v6

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -100,7 +100,7 @@ export default function LoginPage() {
           </div>
 
           <Typography.Title level={2} className="!text-white !m-0 text-center">
-            Cloud Touchsale Backoffice v5
+            Cloud Touchsale Backoffice v6
           </Typography.Title>
           <Typography.Paragraph className="!text-white/80 !mt-2 text-center">
             Достъп само за администратори
